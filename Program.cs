d20f79d30de06d93e74a348d8025d59a21ca8f61
Roman Guydmn <romanguydmn@gmail.com>
2023-01-20 17:53:20 +0400

    Задача 4

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Write("Введите число а ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число c ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if (b > a)
+max = b;
+if (c > b)
+max = c;
+Console.Write($"max = {max}");
